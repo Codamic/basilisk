0742b601f7ef1c681fc918a6c64f09b39d58c90f
Sameer Rahmani <lxsameer@gnu.org>
2016-06-21 16:43:32 -0700

    dataflow improved

--- src/basilisk/tasks/collector/tsetmc/core.clj
+++ src/basilisk/tasks/collector/tsetmc/core.clj
@@ -1,48 +1,58 @@
 (ns basilisk.tasks.collector.tsetmc.core
-  (:require [clojure.core.async :as async]
+  (:require [clojure.core.async
+             :as async
+             :refer [<! >! <!! >!! go go-loop chan close! thread]]
             [clojure-csv.core :as csv]
             [clj-http.client :as http]))
 
 (def site-url {:fa {:index-csv "http://www.tsetmc.com/tsev2/data/MarketWatchInit.aspx?h=0&r=0"}})
 
-(def input-channel (async/chan 1000))
+(def in (chan 1000))
 
 (defrecord TradeSymbol [symbol name tsetmc-id tsetmc-name])
 
+(defn- parse-lines [string]
+  (clojure.string/split string #";"))
 
-(defn fetch-csv
-  "Fetch the CSV file from remote host and add each line to a channel"
-  [output-channel]
-  (async/go
-    (let [buffer (:body (http/get (:index-csv (:fa site-url))))
-          lines (clojure.string/split buffer  #";")]
-      (map #(async/>! output-channel %) lines))))
+(defn csv-fetcher
+  "Fetch the csv and return a channel containing the result."
+  []
+  (thread (:body (http/get (:index-csv (:fa site-url))))))
 
+(defn csv-parser
+  "Parse the csv string and put each record in a channel"
+  [in]
+  (let [out (chan 10000)]
+    (go-loop [data (<! in)]
+      (if (nil? data)
+        (close! data)
+        (>! out  (parse-lines data))))))
 
-(defn create-records
-  "Fetch each line of the CSV from the input-channel and create a record then push it
-  to the output channel."
-  [input-channel output-channel]
-  (async/go-loop []
-    (when-let [line (async/<! input-channel)]
+(defn line-parser
+  "Parse each line and create a record from it and put it in a nre channel"
+  [in]
+  (let [out (chan 10000)]
+    (go-loop [line (<! in)]
       (let [fields (clojure.string/split line #",")]
         (if (= 23 (count fields))
-          (async/>! output-channel
-                    (map->TradeSymbol
-                     {:symbol      (get fields 2)
-                      :name        (get fields 3)
-                      :tsetmc-id   (get fields 0)
-                      :tsetmc-name (get fields 1)}))))
-      (recur))))
+          (>! out (map->TradeSymbol
+                    {:symbol      (get fields 2)
+                     :name        (get fields 3)
+                     :tsetmc-id   (get fields 0)
+                     :tsetmc-name (get fields 1)}))))
+      (recur (<! in)))))
 
-(defn with-each-symbol []
-  (let [lines (take 5 (fetch-csv))]
-    (map create-records lines)))
+
+(defn printer
+  [in]
+  (go-loop [data (<! in)]
+    (println (str ">>" (:name data)))
+    (recur (<! in))))
 
 (defn collect-symbols
   []
-  (let [in (async/chan 10000)
-        rec-chan (async/chan 10000)]
-    (fetch-csv in)
-    (create-records in rec-chan)
-    (async/go-loop [data (async/<! rec-chan)] (println data))))
+  (let [csv (csv-fetcher)
+        lines (csv-parser csv)
+        records (line-parser lines)]
+                                        ;(printer records)
+    ))
