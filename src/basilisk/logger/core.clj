0f237d17f45f510eec644eb505b10334d278ce04
Sameer Rahmani <lxsameer@gnu.org>
2016-08-04 16:40:46 +0430

    basic logger workflow done. Need a real appender

--- src/basilisk/logger/core.clj
+++ src/basilisk/logger/core.clj
@@ -5,26 +5,128 @@
             [clojure.tools.logging :as log]
             [basilisk.logger.kafka :as kafka]
             [environ.core :refer [env]]
-            [basilisk.logger.kafka :refer [kafka-appender]]
+                                        ;[basilisk.logger.kafka :refer [kafka-appender]]
             [com.stuartsierra.component :as component]))
 
+(declare default-output-fn)
 
 (def default-config
   {:min-level :debug
+   :output-fn default-output-fn
    :appenders {:stdout {:min-level :debug,
-                        :output-fn  (fn [data] "NIL")
-                        :fn         (fn [data] "-")}}})
+                        :fn         (fn [data] (println (:output data)))}
+
+               :stdout1 {:min-level :debug,
+                         :output-fn  (fn [data] "NIL")
+                         :fn         (fn [data] (println (:output data)))}}})
+
 (def config (atom default-config))
 
-(defn log
-  [level str]
+(def levels
+  { :trace   10
+    :debug   20
+    :info    40
+    :warn    60
+    :error   80
+    :fatal   100
+    :seppuku 200 })
+
+(defn default-output-fn
+  "Default output generator for logger system. If appenders don't
+  introduce their own appender function, this one will generates the
+  output string."
+  [data]
+  (let [msg (apply str (:args data))]
+    ; I coulda use format instead of str, but format does not supported in cljs
+    (str (:timestamp data) " "
+         (:instance-name data) " "
+         (get data :ns "ns") ":"
+         (get data :fn-name "fn") " "
+         (get data :file "-") ":"
+         (:line data) " ["
+         (-> (:level data)
+             (clojure.string/replace ":" "")
+             clojure.string/upper-case) "] "
+         (apply str (:args data)))))
+
+
+
+(defn- level-value
+  "Return the corresponding integer for given level name."
+  [level-symbol]
+  (get levels level-symbol 0))
+
+(defn- level-matched?
+  "Whether given level is the same or with higher priority of
+  limit level."
+  [level limit]
+  (>= (level-value level) (level-value limit)))
+
+(defn- process-log
+  "Pack and pass the data to output-fn and fn of each appender.
+  If an appender does not have any output-fn the default one
+  will be selected."
+  [data]
+  (let [output-fn   (or (:output-fn data)
+                        (:output-fn @config))
+
+        output      (output-fn data)
+        appender-fn (:fn data)
+        new-data    (merge data {:output output})]
+    (appender-fn new-data)))
+
+(defn- timestamp
+  "It's obvious. isn't it ?"
+  []
+  (let [date (java.util.Date.)]
+    (.format (java.text.SimpleDateFormat. "yyyy-MM-dd H:m:s") date)))
+
+
+
+(defmacro log
+  [level & args]
   (doseq [[name appender-config] (:appenders @config)]
     (let [min-level (or (:min-level appender-config)
-                       (:min-level @config))]
-      (if (>= (level-value level) (level-value min-level))
-        (process-log level str appender-config)))))
+                        (:min-level @config))
+          metadata  (meta  &form)
+          timestamp (timestamp)]
 
-(defn info [str]
-  (log/info str))
+      (when (level-matched? level min-level)
+        (let [data (merge appender-config
+                          {:line      (:line metadata)
+                           :file      (:file metadata)
+                           :ns        (:ns   metadata)
+                           :fn-name   (:name metadata)
+                           :timestamp timestamp
+                           :level     level
+                           :args      args})]
+          `~(process-log data))))))
 
-(log :debug "xzczxczxc")
+(defmacro trace
+  [& args]
+  `(log :trace ~@args))
+
+
+(defmacro debug
+  [& args]
+  `(log :debug ~@args))
+
+(defmacro info
+  [& args]
+  `(log :info ~@args))
+
+(defmacro warn
+  [& args]
+  `(log :warn ~@args))
+
+(defmacro error
+  [& args]
+  `(log :error ~@args))
+
+(defmacro fatal
+  [& args]
+  `(log :fatal ~@args))
+
+(defmacro seppuku
+  [& args]
+  `(log :seppuku ~@args))
